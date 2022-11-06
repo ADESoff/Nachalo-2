da4a5d4abc2507b1b4f69308c00584b1e10c8ebf
ADESoff <117312465+ADESoff@users.noreply.github.com>
2022-11-06 21:06:50 +0500

    Add files via upload

---
+++ Нечетные числа/For + Массив/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = (Convert.ToInt32(Console.ReadLine()));
+int b = (Convert.ToInt32(Console.ReadLine()));
+if (a % 2==0 && b % 2==0)
+{
+    int c = b - a;
+    int f = c / 2;
+    Console.WriteLine(f);
+}
+else
+{
+    int h = b - a;
+    int g =  h / 2 + 1;
+    Console.WriteLine(g);
+}
