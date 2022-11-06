496bc819751671c415a547a750cb7edfb025a1e1
ADESoff <117312465+ADESoff@users.noreply.github.com>
2022-11-06 21:01:50 +0500

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,15 @@
+﻿int one = Convert.ToInt32(Console.ReadLine());
+for (int r = 0; r < 101; r++)
+{
+    if (r == 50) 
+    {      
+        continue;
+    }
+    Console.WriteLine(r);
+    if(r == 100)
+    {
+        int y = 50;
+        Console.WriteLine(y + one);
+
+    }
+}       
