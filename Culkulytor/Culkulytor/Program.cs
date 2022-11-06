8b2d11f60421a7043f6b0b1a730a67125631b8a7
ADESoff <117312465+ADESoff@users.noreply.github.com>
2022-11-06 21:06:11 +0500

    Add files via upload

---
+++ Culkulytor/Culkulytor/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int p = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int v = Convert.ToInt32(Console.ReadLine());
+int z;
+do
+{
+Console.WriteLine("Введите цифру цифра-символ (1-*,2-/,3-,4-+)");
+int o = Convert.ToInt32(Console.ReadLine());
+    z = o;
+    switch (z)
+    {
+        case 1: Console.WriteLine(p * v); break;
+        case 2: Console.WriteLine(p / v); break;
+        case 3: Console.WriteLine(p - v); break;
+        case 4: Console.WriteLine(p + v); break;
+    }
+}
+while (z <= 0 || z >= 5);
