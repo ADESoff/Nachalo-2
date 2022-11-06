4694de6b28b3c678f1ee5b5fda6726312b43f7f7
ADESoff <117312465+ADESoff@users.noreply.github.com>
2022-11-06 20:49:10 +0500

    Add files via upload

---
+++ погода на семь дней/погода на семь дней/Program.cs
@@ -0,0 +1,8 @@
+﻿Random a = new Random();
+string[] r = new string[] {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+for (int i = 0; i < r.Length; i++)
+{
+    int t = a.Next(-10, 30);
+    Convert.ToInt32(t);
+    Console.WriteLine(r[i] + " " + t );
+}
