b04f5d28bca67f537c2b9b74d069180aa55de575
ADESoff <117312465+ADESoff@users.noreply.github.com>
2022-11-06 20:42:43 +0500

    Add files via upload

---
+++ doWail/doWail/Program.cs
@@ -0,0 +1,26 @@
+﻿elka();
+static void elka()
+{
+    string g;
+    do
+    {
+        Console.WriteLine("Введите длинну строки ");
+        int k = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение для Ёлочки");
+        int e = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+        for (int v = 0; v < k; v++)
+        {
+
+            for (int n = k - v; n <= k; n++)
+            {
+                Console.Write(e);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Хотите еще елочку?");
+        Console.WriteLine("Введите да или нет");
+        g = Console.ReadLine();
+    } while (g == "да");
+}
+Console.WriteLine("Спасибо за ёлочку :)");
