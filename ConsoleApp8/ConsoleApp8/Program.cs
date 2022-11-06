41f7bc97ef8077eb45b226288dd97741452b6013
ADESoff <117312465+ADESoff@users.noreply.github.com>
2022-11-06 21:05:33 +0500

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,23 @@
+﻿int summa = Convert.ToInt32(Console.ReadLine());
+if (summa > 200)
+{
+    Console.WriteLine(summa*1.1);
+}
+else
+{
+    if (100 < summa && summa < 200)
+    {
+        Console.WriteLine(summa * 1.07);
+    }
+    else
+    {
+        if (0 < summa && summa < 100)
+        {
+            Console.WriteLine(summa * 1.05);
+        }
+        else
+        {
+            Console.WriteLine("Ваша сумма меньше 1, вы не можете начислить процент.");
+        }
+    }
+}
