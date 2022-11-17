d84c2c5ccfd6068259f72649afc3ddc47d3b9c90
ADESoff <117312465+ADESoff@users.noreply.github.com>
2022-11-17 21:46:25 +0500

    Задача с условием:
    Напечатать «столбиком»:
    а) все целые числа от 20 до 35;
    б) квадраты всех целых чисел от 10 до b (значение b вводится с клавиатуры; b > 10);
    в) третьи степени всех целых чисел от a до 50 (значение a вводится с клавиатуры; a < 50);
    г) все целые числа от a до b (значения a и b вводятся с клавиатуры; b < a).

---
+++ Pflfxf 2/Pflfxf 2/Program.cs
@@ -0,0 +1,22 @@
+﻿for (int c = 20; c <= 35; c++) { Console.WriteLine(c); }
+Console.WriteLine();
+int j = Convert.ToInt32(Console.ReadLine());
+for (int p = 10; p <= j; p++)
+{
+    Console.WriteLine(p * p);
+
+}
+Console.WriteLine();
+int o = Convert.ToInt32(Console.ReadLine());
+for (int r = o; r <= 50; r++)
+{
+    Console.WriteLine(r * r * r);
+
+}
+Console.WriteLine();
+int b = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = b; i <= a; i++)
+{
+    Console.WriteLine(i);
+}
